023248441ea934ccf8c2dd0499307a4f14a2bc93
Greg <ranni91@gmail.com>
2024-10-17 15:57:53 +0300

    * dnd support

---
+++ src/components/VDraganddropabl.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import draggable from 'vuedraggable'
+
+</script>
+
+<template>
+  <draggable
+      v-model="myArray"
+      group="people"
+      @start="drag=true"
+      @end="drag=false"
+      item-key="id">
+    <template #item="{element}">
+      <div>{{element.name}}</div>
+    </template>
+  </draggable>
+</template>
+
+<style scoped lang="scss">
+
+</style>
